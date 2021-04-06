63ef8303b41cac95b2867121f819a6f3f8f1d986
noahhelton98 <noahhelton98@gmail.com>
2021-04-06 08:00:12 -0400

    Made city buttons functional and fixed some styling, having issue with local storage when I refresh - will fix tonight

--- assets/script.js
+++ assets/script.js
@@ -11,44 +11,49 @@ var date = moment().format('MMM Do, YYYY');
 $('#headerFiveDay').hide()
 
 
-//?q=${city}&units=imperial&appid=$8ed513747908d0ab51f99af804b7d62b
+
+
+
+
 var previouslySearched = JSON.parse(localStorage.getItem('cities'))
 if (previouslySearched){
     for (var i =0; i < previouslySearched.length; i++)
-    previousCityEl.append(`<button class = "newcity" class="btn btn-group-vertical">${previouslySearched[i]}</button>`)
+    $('#newButtons').append(`<button class = "newCity" class="btn btn-group-vertical">${previouslySearched[i]}</button>`)
 }
 
 
+
+
 submitEl.on('click', function(event){
-    
-    event.preventDefault()
+    event.preventDefault();
+    if (citySearchEl.val()){
     citiesSearched.push(citySearchEl.val())
     //Save to local storage 
     localStorage.setItem("cities", JSON.stringify(citiesSearched));
     displayWeather(citySearchEl.val());
     displayFiveDay(citySearchEl.val());
-    if (!previouslySearched.includes(citySearchEl.val())){
-        previouslySearchedFunction(citySearchEl.val());
-    }
-    
+    previouslySearchedFunction(citySearchEl.val());
+ 
+}
 })
 
 var previouslySearchedFunction = function(city){
-    previousCityEl.append(`<button class = "newcity" class="btn btn-group-vertical">${city}</button>`)
+    $('#newButtons').append(`<button class = "newCity" class="btn btn-group-vertical">${city}</button>`)
 }
 
-
+//Display the Current weather function 
 var displayWeather =  function (city) {
     mainInfoCity.html("")
     fetch (apiURL + `weather?q=${city}&units=imperial&appid=8ed513747908d0ab51f99af804b7d62b`)
     .then(function(response){
         response.json().then(function(data){
-            console.log(data)
             var temp = data.main.temp;
             var humidity = data.main.humidity;
             var wind = data.wind.speed;
             var icon = data.weather[0].icon
-
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            
             mainInfoCity.append(`<h2>${city}    ${date} 
             <img src = https://openweathermap.org/img/wn/${icon}@2x.png width="50px" height="50px" >
             </h2> 
@@ -56,7 +61,8 @@ var displayWeather =  function (city) {
             <p>Humidity: ${humidity}%</p>
             <p>Wind Speed: ${wind} mph</p>
             <p>`
-            );
+            )
+            getUVIndex(lat, lon);
         })
     })
 
@@ -64,23 +70,53 @@ var displayWeather =  function (city) {
 
 }
 
-
+//Get the UV index and color code it according to value 
+//chose colors from google searching
 var getUVIndex = function(lat, long){
-return true
+    fetch (apiURL + `uvi?lat=${lat}&lon=${long}&units=imperial&appid=8ed513747908d0ab51f99af804b7d62b`)
+    .then (function(response){
+        response.json().then(function(data){
+            index = data.value;
+            if (index >= 8){
+                mainInfoCity.append(
+                    `<p> UV index: <span style=" background-color:red">${index}<span></p>`
+                    )
+            }else if (index < 8 && index >=6 ){
+                mainInfoCity.append(
+                    `<p> UV index: <span style=" background-color:orange">${index}<span></p>`
+                    )
+            }else if (index < 6 && index >= 3){
+                mainInfoCity.append(
+                    `<p> UV index: <span style=" background-color:yellow">${index}<span></p>`
+                    )
+            }else{
+                mainInfoCity.append(
+                    `<p> UV index: <span style=" background-color:green">${index}<span></p>`
+                    )
+            }
+        })
+})
+
 }
 
 var displayFiveDay = function(city){
     $('#headerFiveDay').show();
     fiveDay.html("")
-    //var date = moment().format('MMM Do, YYYY')
+    var dates = []
+    console.log(dates)
+    for (var i = 0; i < 5; i++){
+        var day = moment().add(i, 'd');
+        dates.push(day.format('MM/DD/YYYY'))
+    }
     fetch (apiURL + `forecast?q=${city}&units=imperial&appid=8ed513747908d0ab51f99af804b7d62b`)
     .then (function(response){
         response.json().then(function(data){
             for (var i = 0; i < 5; i++){
                 console.log(data.list[i])
+                
                 fiveDay.append(
                     `<section class = weatherContainer > 
-                    <h3>${date}</h3> 
+                    <h4>${dates[i]}</h4> 
                     <img src = https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png width="50px" height="50px">
                     <p>Temperature: ${data.list[i].main.temp}</p>
                     <p>Humidity: ${data.list[i].main.humidity}%</p>
@@ -94,3 +130,18 @@ var displayFiveDay = function(city){
     })
 }
 
+
+
+
+//Cities added buttons 
+
+var cityBtnsEl = $('#newButtons button')
+
+console.log(cityBtnsEl[0])
+
+cityBtnsEl.on('click', function(event){
+    event.preventDefault()
+    var t = $(this)[0].innerText
+    displayWeather(t);
+    displayFiveDay(t);
+})
